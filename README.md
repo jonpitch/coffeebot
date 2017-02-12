53be3a9b44b3c93cab109e98745da15ae482d908
jonpitch <jon.pitcherella@gmail.com>
2017-02-12 13:25:57 -0500

    basic wiring diagram, instructions, run loop

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # coffeebot
 The percolations are imminent.
+
+# parts
+- Raspberry Pi Zero
+- [MCP23008](https://www.adafruit.com/products/593)
+- [Non-invasive current sensor](https://www.sparkfun.com/products/11005)
+- 2 10k resistors
+- 1 10uF capacitor
+
+# pi setup
+- setup wifi
+- enable ssh
+- enable i2c
+```
+# /boot/config.txt
+dtparam=i2c1=on
+dtparam=i2c_arm=on
+dtparam=i2c=on
+dtparam=i2c_arm_baudrate=10000
+```
+- enable spi (not necessary)?
+- `sudo apt-get update`
+- `sudo apt-get upgrade`
+- `ssh-keygen -t rsa -C pi@coffeebot`
+  - copy contents of `id_rsa.pub` for github: `cat /home/pi/.ssh/id_rsa.pub`
+- `sudo apt-get install git`
+- node
+```
+cd /tmp
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+
+```
+
+# wiring diagram
+![alt text](assets/coffeebot.png "Wiring Diagram")
+[coffeebot.fzz](assets/coffeebot.fzz)
+
+# how it works
+- discharge capacitor, time from full to full changes based on current
+
+# configuration
+coming soon
+
+# usage
+coming soon
--- src/index.js
+++ src/index.js
@@ -6,13 +6,86 @@ const board = new five.Board({
   io: new raspi()
 });
 
+// times in milliseconds
+// TODO configurable
+const brewingThreshold = 200;
+const heatingThreshold = 1700;
+const idleThreshold = 2500;
+const leeway = 200;
+const brewTime = 480000;
+
+// state of coffeepot - 'brewing', 'heating', 'idle'
+let state = 'idle';
+
 board.on('ready', function() {
   
-  console.log('board ready');
+  console.log('coffeebot is online');
+
+  // setup expander
+  const virtual = new five.Board.Virtual(
+    new five.Expander('MCP23008')
+  );
+  
+  // setup ct sensor
+  const current = new five.Sensor({
+    pin: 0,
+    board: virtual,
+    type: 'digital'
+  });
+  
+  let pulses = [];
+  current.on('change', () => {
+    const value = this.value;
+    const now = Date.now();
+    
+    if (value === 1 && pulses.length > 10) {
+      // remove first element, add next time, calculate average
+      pulses.shift();
+      pulses.push(now);
+      const average = pulses.reduce((a, b) => { return a + b; }) / pulses.length;
+      console.log(`average time: ${average}`);
+      
+      if (average < (brewingThreshold + leeway) || 
+          average > (brewingThreshold - leeway) &&
+          state !== 'brewing' &&
+          state !== 'heating'
+      ) {
+        // possible brewing
+        state = 'brewing';
+        
+        // TODO send notification about brewing started
+        console.log('percolations are imminent');
+        
+        // TODO set timeout -> in x minutes, notify coffee is ready
+        setTimeout(() => {
+          console.log('coffee is ready');
+        }, brewTime);
+        
+      } else if (average < (heatingTreshold + leeway) || 
+          average > (heatingTreshold - leeway) &&
+          state !== 'heating' &&
+          state !== 'idle'
+      ) {
+        // possible heating
+        state = 'heating';
+        console.log('coffee pot is heating');
+        
+      } else {
+        // possible idle
+        state = 'idle';
+        console.log('coffee pot is chillin');
+      }
+
+    } else if (value === 1) {
+      // push time onto stack
+      pulses.push(now);
+      console.log('initializing...');
+    }
+  });
   
   // on shutdown
   this.on('exit', function() {
-    console.log('farewell');
+    console.log('coffeebot is offline');
   });
   
   // helpers to add to REPL
